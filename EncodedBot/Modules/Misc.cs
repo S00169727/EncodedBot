46aed5c1ed4d0d70fda35e903206c9a458959328
Conor Fearon <S00169727@mail.itsligo.ie>
2018-02-21 18:11:56 +0000

    Added first module command

---
+++ EncodedBot/Modules/Misc.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodedBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Echo([Remainder] string message)
+        {
+            await Context.Channel.SendMessageAsync(message);
+        }
+    }
+}
